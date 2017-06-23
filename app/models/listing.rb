15f891dc1d6d81432077dd109f90d24ae60e1e21
github username <github email address>
2017-06-23 16:47:34 -0400

    One error left

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,9 @@ class Listing < ActiveRecord::Base
 
   belongs_to :neighborhood
   has_many :reservations
-  belongs_to :host, :class_name => "User"
+  has_many :reviews, through: :reservations
+  has_many :guests, through: :reservations
 
+  belongs_to :host, :class_name => "User"
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   #HOST
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, through: :listings
+
   #GUEST
-  # has_many :reservations, :foreign_key => "guest_id"
+  has_many :reservations, :foreign_key => "guest_id"
+
+  has_many :reviews, through: :reservations
 
   def trips
     Reservation.all
